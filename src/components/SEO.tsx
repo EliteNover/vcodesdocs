3ac7dcb6050d418746976b6e338c607f29a2299c
clqu <76551996+clqu@users.noreply.github.com>
2022-11-23 22:09:41 +0000

    Update SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -15,24 +15,26 @@ const SEO = ({ title , description }: {
       }
     }
   `)
+  
+  const defaultDescription = "Integrate your application with vCodes — Whether you post announcements for your bot, add commands for your bot, or search for bots and users and explore your own imagination.";
 
   return (
     <>
-        <title>{title} | {data.site.siteMetadata.title}</title>
-        {description && <meta name="description" content={description} />}
+        <title>{title}: {"vCodes — API Docs for Developers"}</title>
+        <meta name="description" content={description || defaultDescription} />
         <link rel="icon" href="https://cdn.vcodes.xyz/assets/logo.png" />
-        <meta name="theme-color" content="#090a0f" />
-        <meta name="og:title" content={title} />
-        {description && <meta name="og:description" content={description} />}
+        <meta name="theme-color" content="#7c3aed" />
+        <meta name="og:title" content={title +": vCodes — API Docs for Developers"} />
+        <meta name="og:description" content={description || defaultDescription} />
         <meta name="og:image" content="https://cdn.vcodes.xyz/assets/logo.png" />
         <meta name="og:url" content="https://developers.vcodes.xyz" />
         <meta name="og:type" content="website" />
-        <meta name="og:site_name" content="vCodes" />
+        <meta name="og:site_name" content="vCodes — API Docs for Developers" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@vDiscordBots" />
         <meta name="twitter:creator" content="@vDiscordBots" />
-        <meta name="twitter:title" content={title} />
-        {description && <meta name="twitter:description" content={description} />}
+        <meta name="twitter:title" content={title +": vCodes — API Docs for Developers"} />
+        <meta name="twitter:description" content={description || defaultDescription} />
         <meta name="twitter:image" content="https://cdn.vcodes.xyz/assets/logo.png" />
         <meta name="twitter:image:alt" content="vCodes Logo" />
     </>
